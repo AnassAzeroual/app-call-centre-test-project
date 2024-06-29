a0a260055b23e2865ea89f954520e80e386903e3
Anass Azeroual <azeroual.anass2015@gmail.com>
2024-06-29 20:30:56 +0100

    "Remove auth guard spec file and simplify auth guard implementation"

--- src/app/shared/services/auth.guard.ts
+++ src/app/shared/services/auth.guard.ts
@@ -1,17 +1,11 @@
-import { isPlatformBrowser } from '@angular/common';
-import { PLATFORM_ID, inject } from '@angular/core';
+import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  let authToken = null;
-  const platformId = inject(PLATFORM_ID);
+  let authToken = sessionStorage.getItem('token');
   const router = inject(Router);
 
-  if (isPlatformBrowser(platformId)) {
-    authToken = sessionStorage?.getItem('token') ?? '';
-  }
-
-  if (!!authToken) {
+  if (!authToken) {
     router.navigate(['/login']);
     return false;
   } else {
